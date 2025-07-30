54bb814c791da62534bc2b85f1f0700b462e16e6
Jt Miclat <jtmiclat@gmail.com>
2025-07-30 14:14:54 +0200

    add basics of tle scraping

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # celestrak-historical
-Based on https://www.keiruaprod.fr/blog/2021/05/23/automated-history-of-celestrak-satellite-data.html
+
+A historical flat of celestrak data. Pulled every 3hrs startin July 30, 2025
+
+Inspired from https://www.keiruaprod.fr/blog/2021/05/23/automated-history-of-celestrak-satellite-data.html
+
+## Fetching historical data
+
+```bash
+git clone git@github.com:jtmiclat/celestrak-historical.git
+git pull
+git checkout origin/main
+uv run main.py --noradid=$NORADID --output=example.json
+
+# Example
+# [
+#   {
+#     "date": "2025-07-30T14:18:55+02:00",
+#     "sha": "4f867ff8e38a9ed7a8d1bb8bdd8620d48e03363b",
+#     "tle": "ISS (ZARYA)             \n1 25544U 98067A   25211.15559314  .00012687  00000+0  22820-3 0  9994\n2 25544  51.6363  94.0992 0002130 129.4737   6.6755 15.50214428521827"
+#   }
+# ]
+```
---
+++ main.py
@@ -0,0 +1,58 @@
+import click
+import json
+import git
+from itertools import batched
+import pprint
+
+relative_path = "raw-data/tle-data.txt"
+branch_name = "main"
+show_progress = True  # Set to False to disable progress bar
+
+def parse_tle_file(content: bytes):
+    """Parse TLE file content and yield lines."""
+    lines = content.decode('utf-8').strip().splitlines()
+    tles = {}
+    for tle in batched(lines, 3):
+        norad_id = tle[2].split()[1]
+        tles[norad_id] = '\n'.join(tle).strip()
+    return tles
+
+def get_tle_data(noradid):
+    """Get TLE data for a specific NORAD ID."""
+    repo = git.Repo(".", odbt=git.GitDB)
+    commits = reversed(list(repo.iter_commits(branch_name, paths=[relative_path])))
+    if show_progress:
+        progress_bar = click.progressbar(commits, show_pos=True, show_percent=True)
+    for commit in commits:
+        if progress_bar:
+            progress_bar.update(1)
+        try:
+            content = commit.tree[relative_path].data_stream.read()
+            tles = parse_tle_file(content)
+            tle = tles[noradid]
+            yield commit.committed_datetime, commit.hexsha, tle
+        except KeyError:
+            continue
+
+@click.command()
+@click.option('--output', default="-",  help='Path to the file in the')
+@click.option('--noradid', required=True, help='NORAD ID of the satellite')
+def main(output, noradid):
+    info = get_tle_data(noradid)
+    dedup = set()
+    results = []
+    for date, sha, tle in info:
+        if tle in dedup:
+            # already seen this TLE, skip it. this usually means the tle was not updated
+            continue
+        results.append(dict(date=date.isoformat(), sha=sha, tle=tle))
+        dedup.add(tle)
+    if output == "-":
+        click.echo("\n")
+        click.echo(json.dumps(results, indent=2))
+    else:
+        with open(output, 'w') as f:
+            json.dump(results, f, indent=2)
+        
+if __name__ == "__main__":
+    main()
